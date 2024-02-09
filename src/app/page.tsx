bec0a696cf0df3db3ed08290c24910da9988cb41
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-02-09 15:21:47 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import { useState } from "react";
 import UserItem from 'useritem';
 
-const AVATAR_URL = "https://substackcdn.com/image/fetch/w_96,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fb60dc67d-fd59-47eb-84f1-e2ba033783d0_545x545.png"
+const AVATAR_URL = "https://substackcdn.com/image/fetch/w_96,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fb60dc67d-fd59-47eb-84f1-e2ba033783d0_545x545.png";
 
 export default function Home() {
   const [data, setData] = useState<any>({
@@ -41,27 +41,27 @@ export default function Home() {
       title: "Settings",
       icon: <Settings />
     }
-  ])
+  ]);
 
   const handleChange = ({ v, target }: any) => {
     if (target === 'avatarUrl') {
       return setData((prev: any) => ({ ...prev, [target]: data.avatarUrl ? "" : data.avatarUrl.length === 0 ? AVATAR_URL : "" }))
     }
     setData((prev: any) => ({ ...prev, [target]: v }))
-  }
+  };
 
   return <div className="flex flex-col gap-8">
     <header className="flex flex-col items-center justify-center pt-8 pb-2">
       <h1>useritem</h1>
       <p className="lg:text-[20px] text-neutral-400 mb-0 whitespace-normal">A simple, fully customizable component for displaying your users.</p>
     </header>
-    <main className="grid gap-4 grow pb-12">
+    <main className="grid gap-4 pb-12 grow">
       <div className="flex items-center justify-center mb-6">
         <UserItem {...data} />
       </div>
       <div className="grid gap-4">
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 lg:w-[600px] align-top py-4 mx-auto content-start">
-          {["avatar", "avatarUrl", "border", "disabled", "loading", "online", "shadow", "squared", "status"].map((target: any, key: number) => <div key={key} className="flex items-center justify-center gap-1">
+          {["avatar", "avatarUrl", "border", "disabled", "infos", "loading", "online", "shadow", "squared", "status"].map((target: any, key: number) => <div key={key} className="flex items-center justify-center gap-1">
             <Switch
               id={target}
               checked={data[target]}
@@ -90,4 +90,4 @@ export default function Home() {
       backgroundPosition: 'center center'
     }} />
   </div>
-}
+};
