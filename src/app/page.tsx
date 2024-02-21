d05e7e8153bedcdc3d9451afa4930e588fb1cc77
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-02-21 20:30:18 +0400

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import ReactIcon from "@/components/Icons/React";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
-import { CreditCardIcon, Github, Settings, User } from "lucide-react";
+import { Github, Shuffle } from "lucide-react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import Logo from '../../public/logo.png'
+import Logo from '../../public/logo.png';
 const UserItem = dynamic(() => import("useritem"), { ssr: false })
 
 const AVATAR_URL = "https://substackcdn.com/image/fetch/w_96,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fb60dc67d-fd59-47eb-84f1-e2ba033783d0_545x545.png";
@@ -21,7 +21,6 @@ export default function Home() {
     border: true,
     description: "codewithguillaume@gmail.com",
     disabled: false,
-    dropdown: false,
     icon: false,
     infos: true,
     loading: false,
@@ -34,18 +33,20 @@ export default function Home() {
     verified: true,
   });
 
-  const [items] = useState<any>([
-    {
-      title: "Profile",
-      icon: <User />
-    }, {
-      title: "Billing",
-      icon: <CreditCardIcon />
-    }, {
-      title: "Settings",
-      icon: <Settings />
+  const randomize = async () => {
+    try {
+      const response = await fetch('/api/random');
+      if (!response.ok) {
+        throw new Error('Failed to fetch random data');
+      }
+      const data = await response.json();
+      console.log(data)
+      setData(data);
+    } catch (error) {
+      console.error('Error fetching random data:', error);
     }
-  ]);
+  };
+
 
   const handleChange = ({ v, target }: any) => {
     if (target === 'avatarUrl') {
@@ -72,11 +73,14 @@ export default function Home() {
         <Image src={Logo} alt="UserItem" />
       </div>
       <h1>useritem</h1>
-      <p className="lg:text-[20px] text-neutral-400 mb-0 whitespace-normal">A fully customizable component to display your users.</p>
+      <p className="mb-0 whitespace-normal text-neutral-400">A fully customizable component to display your users.</p>
     </header>
     <main className="grid gap-4 pb-12 grow">
-      <div className="flex items-center justify-center mb-6">
-        <UserItem {...data} onClick={() => alert("hello there!")} />
+      <div className="flex items-center justify-center gap-4 mb-6">
+        <div className="scale-105">
+          <UserItem {...data} onClick={() => alert("hello there!")} />
+        </div>
+        <Shuffle onClick={() => randomize()} className="cursor-pointer hover:text-black text-neutral-500" />
       </div>
       <div className="grid gap-4">
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 lg:w-[600px] align-top py-4 mx-auto content-start">
